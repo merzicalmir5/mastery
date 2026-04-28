baa07a80e0e065a8d4d303b10d1b4e65486a24d9
Almir Merzic <almir.merzic@finshark.io>
2026-04-29 01:48:52 +0200

    Added features folder and inside will be all features that i will be implementing, so the first one is login.

---
+++ web/src/features/login/login.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  private readonly fb = inject(FormBuilder);
+
+  readonly loginForm = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  submit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Login payload:', this.loginForm.getRawValue());
+  }
+}
