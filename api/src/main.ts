2d6017ef99f3e2e675b0bd9ee130e4af22c00a5c
Almir Merzic <almir.merzic@finshark.io>
2026-05-02 02:49:15 +0200

    Fixed backend deployment.

--- api/src/main.ts
+++ api/src/main.ts
@@ -43,6 +43,7 @@ async function bootstrap() {
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
